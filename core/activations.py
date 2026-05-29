Fixed negative cost by ignoring labels in normalization; added L2 regularization, early stopping to training; started working on saving model states

core/activations.py
@@ -4,11 +4,11 @@ def linear_activation(z):
     return z
 
 
-def reLU(z):
+def relu_activation(z):
     return np.maximum(0, z)
 
 
-def sigmoid(z):
+def sigmoid_activation(z):
     return 1 / (1 + (np.exp(-z)))
 
 
@@ -16,12 +16,12 @@ def linear_deriv(z):
     return 1
 
 
-def reLU_deriv(z):
+def relu_deriv(z):
     return (z > 0).astype(float)
 
 
 def sigmoid_deriv(z):
-    s = sigmoid(z)
+    s = sigmoid_activation(z)
     return s * (1 - s)
 
 class Activation:

core/layers/dense.py
@@ -18,7 +18,7 @@ class Dense:
         self.A = self.a_func(self.Z)
         return self.A
 
-    def compute_gradients(self, dA_back):
+    def compute_gradients(self, dA_back, lambda_reg=0.01):
         # Number of examples in the current batch
         m = self.a_prev.shape[0]
 
@@ -29,7 +29,7 @@ class Dense:
         # print(self.dZ.shape)
 
         #
-        self.dW = self.a_prev.T @ self.dZ / m   # (count_prev, samples) @ (samples, count)  = (count_prev, count) AKA (input_size, count) = Shape(W)
+        self.dW = (self.a_prev.T @ self.dZ / m) + (lambda_reg / m) * self.W  # (count_prev, samples) @ (samples, count)  = (count_prev, count) AKA (input_size, count) = Shape(W)
         self.dB = np.sum(self.dZ, axis=0, keepdims=True) / m
 
         #
@@ -40,6 +40,16 @@ class Dense:
     def update_params(self, lr):
         self.W -= lr * self.dW
         self.B -= lr * self.dB
+
+    def save_state(self):
+        return {
+            "W" : self.W.copy(),
+            "B" : self.B.copy()
+        }
+
+    def load_state(self, state):
+        self.W = state["W"].copy()
+        self.B = state["B"].copy()
 
 
 